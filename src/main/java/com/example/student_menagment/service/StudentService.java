2d839c1d47e2f17b99fa0815cbd1a89b9afa5837
Rinor Selmani <rinorselmani99@gmail.com>
2025-07-24 16:27:13 +0200

    Implemented StudentService with crud

---
+++ src/main/java/com/example/student_menagment/service/StudentService.java
@@ -0,0 +1,43 @@
+package com.example.student_menagment.service;
+
+import com.example.student_menagment.model.Student;
+import com.example.student_menagment.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student updateStudent(Long id, Student updatedStudent) {
+        return studentRepository.findById(id).map(student -> {
+            student.setName(updatedStudent.getName());
+            student.setEmail(updatedStudent.getEmail());
+            student.setCourse(updatedStudent.getCourse());
+            return studentRepository.save(student);
+        }).orElse(null);
+    }
+}
