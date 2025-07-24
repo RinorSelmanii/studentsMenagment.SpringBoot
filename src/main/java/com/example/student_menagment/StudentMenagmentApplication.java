68fe6bb56d6d77d5ac118db654f5180cdb401bee
Rinor Selmani <rinorselmani99@gmail.com>
2025-07-24 15:50:53 +0200

    add structure

---
+++ src/main/java/com/example/student_menagment/StudentMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_menagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMenagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMenagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_menagment/StudentMenagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_menagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMenagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
