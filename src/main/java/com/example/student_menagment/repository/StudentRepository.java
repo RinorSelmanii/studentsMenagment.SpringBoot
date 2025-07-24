5ec319f106a4912088d929c4d1ca2e547fa64ef1
Rinor Selmani <rinorselmani99@gmail.com>
2025-07-24 16:19:27 +0200

    add rep interface

--- src/main/java/com/example/student_menagment/repository/StudentRepository.java
+++ src/main/java/com/example/student_menagment/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.example.student_menagment.repository;
 
-public class StudentRepository {
-    
+import com.example.student_menagment.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
